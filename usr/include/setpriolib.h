6f30a2d150dac83c8759d0db883b40fee2dda3cf
efraimrodrigues <efraimnaassom@gmail.com>
2015-11-07 13:45:21 -0300

    Update setpriolib.h
    Este arquivo define que o sistema passará a mensagem para o servidor PM e requisitará que o serviço SETPRIO seja executado.

--- usr/include/setpriolib.h
+++ usr/include/setpriolib.h
@@ -1,16 +1,13 @@
 #include <lib.h>
-//#define setprio do_setprio
+
 #include <unistd.h>
 
-// função com modificador PUBLIC
-int setprio(int this_pid, int this_priority) {
+int setprio(pid_t process, int priority) {
 
 	message m;
 
-//	sys_nice(this_pid, this_priority);
-
-	m.m1_i1 = this_pid;
-	m.m1_i2 = this_priority;
+	m.m1_i1 = process;
+	m.m1_i2 = priority;
 
 	return ( _syscall(PM_PROC_NR, SETPRIO, &m));
 }
